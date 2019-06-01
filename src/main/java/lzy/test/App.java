2c30d25de56625341a578b2e34c818d76f057e62
Lu Zhou Yuan <lzy_0828@163.com>
2019-06-01 09:08:36 +0800

    first commit

---
+++ src/main/java/lzy/test/App.java
@@ -0,0 +1,97 @@
+package lzy.test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static java.util.Collections.emptyList;
+import static java.util.stream.Collectors.toList;
+
+public class App {
+    public static void main(String[] args) {
+        if (args != null && args.length > 0) {
+            if (args[0].trim().toLowerCase().equals("--help") || args[0].trim().toLowerCase().equals("-h"))
+                System.out.print("Please enter your digits with comma like the following example: 0,4,18");
+            else
+                new LetterPrinter(args[0].trim()).print();
+        }
+    }
+}
+
+class LetterPrinter {
+    protected String[] digitLetters=new String[]{"","","abc","def","ghi","jkl","mno","pqrts","tuv","wxyz"};
+    final String args;
+
+    public LetterPrinter(String args) {
+        this.args = args;
+    }
+
+    public void print() {
+        String[] representLetters;
+        try {
+            representLetters = getRepresentLetters();
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.err.printf("Please enter digit from 0 to %d.",digitLetters.length);
+            return;
+        }
+        if (representLetters == null) {
+            System.err.print("Represent letters not found!");
+            return;
+        }
+
+        List[] letters = getLetters(representLetters);
+        getCartesianProduct(letters).forEach(x-> {
+            System.out.print(x);
+            System.out.print(" ");
+        });
+    }
+
+    private List<String> getCartesianProduct(List<String>... a) {
+        if (a.length >= 2) {
+            List<String> product = a[0];
+            for (int i = 1; i < a.length; i++) {
+                product = getCartesianProduct(product, a[i]);
+            }
+            return product;
+        }
+
+        return a[0];
+    }
+
+    private List<String> getCartesianProduct(List<String> a, List<String> b) {
+        return Optional.of(a.stream()
+                .map(e1 -> Optional.of(
+                        b.stream()
+                                .map(e2 ->
+                                        Arrays.asList(e1, e2))
+                                .collect(toList())).orElse(emptyList()))
+                .flatMap(List::stream)
+                .map(chars-> String.join("",chars) )
+                .collect(toList())).orElse(emptyList());
+    }
+
+    private List[] getLetters(String[] representLetters) {
+        return Arrays.asList(representLetters)
+                .stream()
+                .map(x -> x.chars()
+                        .mapToObj(e->(char)e)
+                        .map(e->e.toString())
+                        .collect(Collectors.toList()))
+                .collect(toList())
+                .toArray(new List[0]);
+    }
+
+    private String[] getRepresentLetters() {
+
+        return Arrays.stream(args.split(","))
+                .map(x-> {
+                            Integer i =Integer.parseInt(x);
+                            return digitLetters[i];
+                        }
+                )
+                .toArray(size->new String[size]);
+    }
+
+}
---
+++ src/test/java/lzy/test/AppTest.java
@@ -0,0 +1,96 @@
+package lzy.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class AppTest
+{
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    @Before
+    public void set() {
+
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @Test
+    public void mainTestWithHelp(){
+        App.main(new String[]{"--help"});
+        assertTrue(StringUtils.isNotEmpty(outContent.toString()));
+    }
+
+    @Test
+    public void mainTestWithShortHelpCommand(){
+        App.main(new String[]{"-h"});
+        assertTrue(StringUtils.isNotEmpty(outContent.toString()));
+    }
+
+
+    @Test
+    public void mainWithNullArgs(){
+        App.main(null);
+        assertTrue(StringUtils.isAllEmpty(outContent.toString() ));
+    }
+
+    @Test
+    public void mainTestWithOutlierArg(){
+        App.main(new String[]{"0,100"});
+        assertTrue(StringUtils.isNotEmpty(errContent.toString()));
+    }
+
+    @Test
+    public void mainTestThatHasOutput(){
+        App.main(new String[]{"5"});
+        assertTrue(StringUtils.split(outContent.toString()," " ).length>0);
+    }
+
+    @Test
+    public void mainTestWithoutOutput(){
+        App.main(new String[]{"1"});
+        assertTrue(StringUtils.isAllEmpty(outContent.toString() ));
+    }
+
+    @Test
+    public void letterPrinterTestWithEmptyLetter()
+    {
+        LetterPrinterTest lp = new LetterPrinterTest("0,1,2");
+        String[] digitLetters=new String[]{"","ab","c"};
+        lp.setDigits(digitLetters);
+
+        lp.print();
+        assertTrue(StringUtils.isAllEmpty(outContent.toString() ));
+    }
+
+    @Test
+    public void letterPrinterTest()
+    {
+        LetterPrinterTest lp = new LetterPrinterTest("1,2,3");
+        String[] digitLetters=new String[]{"","ab","c","de"};
+        lp.setDigits(digitLetters);
+
+        lp.print();
+        assertTrue(StringUtils.split(outContent.toString()," " ).length==(2*1*2));
+    }
+
+    class LetterPrinterTest extends LetterPrinter{
+
+        public LetterPrinterTest(String args) {
+            super(args);
+        }
+
+
+        public void setDigits(String[] digitLetters) {
+            super.digitLetters = digitLetters;
+        }
+    }
+
+
+
+}
